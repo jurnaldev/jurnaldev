b0bc2fef4a2b5d3f8f0e46598c19b8508223d9f8
jurnaldev <me@fahmidyt.dev>
2026-04-30 00:20:51 +0700

    feat: add CORS headers for Giscus theme CSS

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  headers: async () => {
+    return [
+      {
+        source: "/giscus-theme-:theme.css",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
